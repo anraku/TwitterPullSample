edc03dca6cbd4922111f00d29fdd0f5b301bdd49
anraku <anraku90@gmail.com>
2017-10-20 00:49:53 +0900

    第6章 p163

--- api/polls.go
+++ api/polls.go
@@ -8,3 +8,29 @@ type poll struct {
 	Options []string       `json:"options"`
 	Results map[string]int `json:"result,omitempty"`
 }
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlePollsGet(w, r)
+		return
+	case "POST":
+		handlePollsPost(w, r)
+		return
+	case "DELETE":
+		handlePollsDelete(w, r)
+		return
+	}
+	// 未対応のHTTPメソッド
+	respondHTTPErr(w, r, http.StatusNotFound)
+}
+
+func handlePollsGet(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+func handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+func handlePollsDelete(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
